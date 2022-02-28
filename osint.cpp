306b6d4989e18a8ca157e9729283f266f2a79ec6
David Betz <dbetz@xlisper.com>
2022-02-28 18:33:27 -0500

    Split the 9P filesystem interface into a separate file.

--- osint.cpp
+++ osint.cpp
@@ -5,10 +5,6 @@
 #include "panel_leds.h"
 #include "panel_switches.h"
 
-extern "C" {
-    #include "fs9p.h"
-}
-
 void serial_putchar(int ch)
 {
 	putchar_raw(ch);
@@ -18,104 +14,6 @@ int serial_getchar()
 {
 	int ch = getchar_timeout_us(0);
 	return ch == PICO_ERROR_TIMEOUT ? -1 : ch;
-}
-
-fs9_file f;
-bool finit = false;
-
-// receive 1 byte
-static unsigned int zdoGet1()
-{
-    int c;
-    do {
-        c = serial_getchar();
-    } while (c < 0);
-    return c;
-}
-
-// receive an unsigned long
-static unsigned int zdoGet4()
-{
-    unsigned r;
-    r = zdoGet1();
-    r = r | (zdoGet1() << 8);
-    r = r | (zdoGet1() << 16);
-    r = r | (zdoGet1() << 24);
-    return r;
-}
-
-// send a buffer to the host
-// then receive a reply
-// returns the length of the reply, which is also the first
-// longword in the buffer
-//
-// startbuf is that start of the buffer (used for both send and
-// receive); endbuf is the end of data to send; maxlen is maximum
-// size
-static int plain_sendrecv(uint8_t *startbuf, uint8_t *endbuf, int maxlen)
-{
-    int len = endbuf - startbuf;
-    uint8_t *buf = startbuf;
-    int i = 0;
-    int left;
-    unsigned flags;
-
-    startbuf[0] = len & 0xff;
-    startbuf[1] = (len>>8) & 0xff;
-    startbuf[2] = (len>>16) & 0xff;
-    startbuf[3] = (len>>24) & 0xff;
-
-    if (len <= 4) {
-        return -1; // not a valid message
-    }
-    // loadp2's server looks for magic start sequence of $FF, $01
-    serial_putchar(0xff);
-    serial_putchar(0x01);
-    while (len>0) {
-        serial_putchar(*buf++);
-        --len;
-    }
-    len = zdoGet4();
-    startbuf[0] = len & 0xff;
-    startbuf[1] = (len>>8) & 0xff;
-    startbuf[2] = (len>>16) & 0xff;
-    startbuf[3] = (len>>24) & 0xff;
-    buf = startbuf+4;
-    left = len - 4;
-    while (left > 0 && i < maxlen) {
-        buf[i++] = zdoGet1();
-        --left;
-    }
-    return len;
-}
-
-int dsk_init(const char *path)
-{
-    if (fs_init(plain_sendrecv) == 0) {
-        if (fs_open(&f, path, FS_MODE_RDWR) == 0) {
-            finit = true;
-            return 0;
-        }
-    }
-    return -1;
-}
-
-int dsk_seek(unsigned int addr)
-{
-    //printf("seek %u\n", addr);
-    return finit ? fs_seek(&f, (uint64_t)addr) : -1;
-}
-
-size_t dsk_read(uint8_t *buffer, size_t size)
-{
-    //printf("read %d\n", size);
-    return finit ? fs_read(&f, buffer, size) : -1;
-}
-
-size_t dsk_write(uint8_t *buffer, size_t size)
-{
-    //printf("write %d\n", size);
-    return finit ? fs_write(&f, buffer, size) : -1;
 }
 
 ms_time_t mstime()
